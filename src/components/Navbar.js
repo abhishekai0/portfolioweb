10093dc34609c5bc65d96769a5acb5eca72e645b
abhishekkumar(darkcoder) <161643090+abhishekai0@users.noreply.github.com>
2024-06-25 19:22:04 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -90,7 +90,7 @@ const CustomNavbar = () => {
                                 <NavDropdown.Item href="https://chat.whatsapp.com/JdbD2Hx8Mi2FiZqMy4hgfH">
                                     whatshapp
                                 </NavDropdown.Item>
-                                <NavDropdown.Item href="https://www.github.com/abhishekai0.git">
+                                <NavDropdown.Item href="https://github.com/abhishekai0">
                                     github
                                 </NavDropdown.Item>
                                 <NavDropdown.Item href="https://www.instagram.com/kumar.abhishek44279/?igsh=MXJ0aDV6dGpsNjIzNg%3D%3D">
